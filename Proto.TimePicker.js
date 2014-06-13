a06aa613f70ed97e7fbabd859c1893f3e0b70d29
rajeshgoyalg <rajeshgoyalg@gmail.com>
2014-06-13 18:09:41 +0530

    Update Proto.TimePicker.js
    By pressing down key, time option moves down in the list but scroll bar does not moves down. Due to this, selected time option is not visible.
    
    Steps to Reproduce:
    1. Click on the time picker text box.
    5. Scroll down using the down key.
    
    ACTUAL RESULT:
    
    The time selected moves down the list but the list does not scroll down to show the selected option on pressing the down key.
    
    EXPECTED RESULT:
    
    The time selected should moves down the list and list should scroll down to show the selected option on pressing the down key.

--- Proto.TimePicker.js
+++ Proto.TimePicker.js
@@ -167,7 +167,7 @@
 							// Loop to next item.
 							$selected.removeClassName(selectedClass);
 							prev = $tpList.down("li:last").addClassName(selectedClass)[0];
-							$tpDiv.scrollTop = prev.positionedOffset().top - prev.getHeight;
+							$tpDiv.scrollTop = prev.positionedOffset().top - prev.getHeight();
 						}
 						e.stop();
 						return false;
@@ -180,7 +180,7 @@
 						if (next) {
 							next.addClassName(selectedClass);
 							$selected.removeClassName(selectedClass);
-							if (next.positionedOffset().top + next.getHeight() > top + $tpDiv.getHeight) {
+							if (next.positionedOffset().top + next.getHeight() > top + $tpDiv.getHeight()) {
 								$tpDiv.scrollTop = top + next.getHeight();
 							}
 						}
